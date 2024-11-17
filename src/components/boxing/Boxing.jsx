8f04a1f09329488663395e76564c7e380af9a59e
Fahime <nafisurr93@gmail.comm>
2024-11-17 23:36:40 +0600

    demo

--- src/components/boxing/Boxing.jsx
+++ src/components/boxing/Boxing.jsx
@@ -3,16 +3,42 @@ import { Container } from "react-bootstrap";
 import "./boxing.css";
 import Slider from "react-slick";
 import slider1 from "../../assets/oo.webp"
-import slider2 from "../../assets/slider 4.png"
-import slider3 from "../../assets/prom.jpg"
+import slider2 from "../../assets/slider 5.jpg"
+import slider3 from "../../assets/slider 7.avif"
+import { GrFormNext } from "react-icons/gr";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import ScrollToTop from "react-scroll-to-top";
+
+
+
+function SampleNextArrow(props) {
+  const {onClick } = props;
+  return (
+    <div className="naxt" onClick={onClick}
+    > <GrFormNext /> </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div className="prev" onClick={onClick}
+    >< MdKeyboardArrowLeft/></div>
+  );
+}
+
 
 const Boxing = () => {
 
   var settings = {
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
-    dots: true
+    dots: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000
   };
 
 
@@ -35,6 +61,7 @@ const Boxing = () => {
           </Slider>
         </section>
       </Container>
+      <ScrollToTop smooth />
     </section>
   );
 };
