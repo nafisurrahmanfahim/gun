629ec1a6b396a451c414cc648c4fdc7e0e080987
Fahime <nafisurr93@gmail.comm>
2024-11-17 20:02:08 +0600

    demo

--- src/components/boxing/Boxing.jsx
+++ src/components/boxing/Boxing.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import "./boxing.css";
-import boxing1 from "../../assets/oo.webp"
 import Slider from "react-slick";
-import slider2 from "../../assets/slider 1.avif"
+import slider1 from "../../assets/oo.webp"
+import slider2 from "../../assets/slider 4.png"
+import slider3 from "../../assets/prom.jpg"
 
 const Boxing = () => {
 
@@ -18,19 +19,21 @@ const Boxing = () => {
   return (
     <section id="boxing">
       <Container>
-        <Slider {...settings}>
-          <div className="images">
-            <img src={slider2} alt="" />
-          </div>
+        <section className="main">
+          <Slider {...settings}>
+            <div className="images">
+              <img src={slider2} alt="" />
+            </div>
 
-          <div className="images">
-            <img src={boxing1} alt="" />
-          </div>
+            <div className="images">
+              <img src={slider1} alt="" />
+            </div>
 
-          <div className="images">
-            <img src={boxing1} alt="" />
-          </div>
-        </Slider>
+            <div className="images">
+              <img src={slider3} alt="" />
+            </div>
+          </Slider>
+        </section>
       </Container>
     </section>
   );
